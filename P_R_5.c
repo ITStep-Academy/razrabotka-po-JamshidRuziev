8396732d9ea697a6dcc411d02e1782d126cf9fab
JamshidRuziev <69230380+JamshidRuziev@users.noreply.github.com>
2020-08-10 11:33:42 +0500

    Update P_R_5.c

--- P_R_5.c
+++ P_R_5.c
@@ -1,17 +1,15 @@
-/*5.Записать логическое выражение, определяющее какая дробь больше А/B или C/D. */
+/*5.Записать логическое выражение, определяющее какая дробь больше А/B или C/D*/
 #include <stdio.h>
 #include <stdbool.h>
 
 int main ()
 {
-	int a,b,c,d;
-	scanf("%d",&a);
-	scanf("%d",&b);
+	float a,b,c,d;
+	scanf("%lf",&a,&b);
+	scanf("%lf",&c,&d);
 	
 
-
-	
-	printf("a/b > cd %d\n",(a/b > c/d ));
-	printf("a/b < cd %d\n",(a/b < c/d ));
+	printf("a/b > c/d =  %d\n",(a/b > c/d ));
+	printf("a/b < c/d =  %d\n",(a/b < c/d ));
 	return 0;
 }
